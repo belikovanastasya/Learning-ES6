c5ad4b350659c902b5644ae9df889fb327c39871
belikovanastasya <belikovaava@gmail.com>
2017-03-22 22:21:46 +0200

    destructoring arrays

---
+++ src/destructuring.js
@@ -0,0 +1,14 @@
+/*let arr = ['mama', 'papa', 'son'];
+
+let [m, p, s] = arr;
+console.log (m, p, s)*/
+
+
+
+let [m, p, s] = ['mama', 'papa', 'son'];
+console.log (m, p, s)
+
+function score ([low, heigh]){
+    console.log(low, heigh)
+}
+score([2,3]);
